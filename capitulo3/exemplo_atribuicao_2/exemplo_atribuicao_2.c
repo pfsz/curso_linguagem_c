29fea2b168fbcc1043306ae6098aa9e86324b42f
leobiscassi <leo.biscassi@gmail.com>
2015-08-12 23:40:43 -0300

    Capitulo 3: Operador de atribuição

---
+++ capitulo3/exemplo_atribuicao_2/exemplo_atribuicao_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	float x, y, z;
+	x = y = z = 5;
+
+	printf("x = %f\n", x);
+	printf("y = %f\n", y);
+	printf("z = %f\n", z);
+
+	return 0;
+}
